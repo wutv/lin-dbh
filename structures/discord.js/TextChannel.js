3b4a649a9b60c183fa2f6a621393d8a7aa719dab
Lin <86250346+wutv@users.noreply.github.com>
2021-06-22 11:57:17 +0200

    Update TextChannel.js

--- structures/discord.js/TextChannel.js
+++ structures/discord.js/TextChannel.js
@@ -10,7 +10,7 @@ module.exports = {
                     name: title,
                     iconURL: which.toLowerCase() == "success" ? "https://uxwing.com/wp-content/themes/uxwing/download/01-user_interface/check-mark-circle.png" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd6K0bsxE2e3walXf3tIiCGnX_o2zwcjdUAUCm0E54Hkk3PGi2SkWWQhWK_mi9tM0c1-E&usqp=CAU"
                 };
-                optionsRaw["description"] = description;
+                options["description"] = description;
 
                 const embed = new MessageEmbed(options);
                 const msg = await this.send(embed);
