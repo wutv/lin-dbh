e87814d115a2bf47d213e3b80eee350e9b93d2db
Lin <86250346+wutv@users.noreply.github.com>
2021-06-22 09:47:15 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,15 @@
+const DanBotHosting = require("danbot-hosting");
+
 module.exports = {
     name: "ready",
     once: true,
-    exec (client) {
+    async exec (client) {
         client.logger.success("LinBot successfully logged in!")
+
+        const API = new DanBotHosting.Client(client.config.apiKey, client);
+ 
+        let initalPost = await API.autopost();
+
+        if (initialPost) throw initialPost;
     }
 }
