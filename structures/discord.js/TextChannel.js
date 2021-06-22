9751bfb106ca2cdd27eb0cef56005ee7c6d792af
Lin <86250346+wutv@users.noreply.github.com>
2021-06-22 11:59:21 +0200

    Update TextChannel.js

--- structures/discord.js/TextChannel.js
+++ structures/discord.js/TextChannel.js
@@ -6,7 +6,7 @@ module.exports = {
             async sendCustom(which, title, description, optionsRaw) {
                 if (!["success", "error"].includes(which)) throw new Error("Invalid custom send type provided.");
                 let options = optionsRaw ?? {};
-                optionsRaw["author"] = {
+                options["author"] = {
                     name: title,
                     iconURL: which.toLowerCase() == "success" ? "https://uxwing.com/wp-content/themes/uxwing/download/01-user_interface/check-mark-circle.png" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd6K0bsxE2e3walXf3tIiCGnX_o2zwcjdUAUCm0E54Hkk3PGi2SkWWQhWK_mi9tM0c1-E&usqp=CAU"
                 };
